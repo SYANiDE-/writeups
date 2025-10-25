c14fe1c4c53b86470020561cf03f6b6ac481bf8a
SYANIDE- <chase.hatch.business@gmail.com>
2025-10-25 14:22:31 -0700

    Image references pre-commit hook

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,7 +2,7 @@
 
 ## Find all referenced pastedimages and add them to docs, renaming spaces to underscore
 for item in $(find . -name "*.md"); do 
-    found=($(grep -FPo "Pasted image .+.png" $item | tr ' ' '_')); 
+    found=($(grep -Po "Pasted image .+.png" $item | tr ' ' '_')); 
     for meti in "${found[@]}"; do 
         if [[ ! -f docs/$meti ]]; then 
             cp ../../"${meti//_/ }" docs/$meti
@@ -12,7 +12,7 @@ done
 
 ## change all references from "Pasted image .+png" to "Pasted_image_.+png" replacing any spaces
 for item in $(find . -name "*.md"); do 
-    IFS=$'\n' found=($(grep -FPo "Pasted image .+.png" $item))
+    IFS=$'\n' found=($(grep -Po "Pasted image .+.png" $item))
     for meti in "${found[@]}"; do 
         repl="${meti// /_}"
         sed -re "s/$meti/$repl/g" $item -i
